6e9eee60ff8667edcf55394a829ce61bdb6b74b1
SAHILKHAN7001 <106735502+SAHILKHAN7001@users.noreply.github.com>
2022-09-14 11:39:27 +0530

    toggle

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener("click",function() {
-    document.body.classList.add("nav-open");
+    document.body.classList.toggle("nav-open");
 })
 
 navLinks.forEach(link => {
