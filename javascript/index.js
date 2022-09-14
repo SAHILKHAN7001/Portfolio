77b91aba4006438afb9b6af3dfe1125f1e8c7aa4
SAHILKHAN7001 <106735502+SAHILKHAN7001@users.noreply.github.com>
2022-09-14 11:32:44 +0530

    PortFolio

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+let title = document.getElementById("title").innerHTML = `< Sahil Khan >`;
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener("click",function() {
+    document.body.classList.add("nav-open");
+})
+
+navLinks.forEach(link => {
+    link.addEventListener("click",function() {
+        document.body.classList.remove("nav-open")
+    })
+})
+
